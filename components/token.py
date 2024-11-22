a7079035b829c73e5cc64740643364a3af434e9a
alina-ellevation <alina.psaros@ellevationeducation.com>
2024-11-22 02:19:12 -0500

    add token base class

---
+++ components/token.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Token(Enum):
+    white = "#FFFFFF"
+    red = "#FF0000"
+    blue = "#0000FF"
+    green = "#00FF00"
+    black = "#000000"
+    purple = "#800080"
+    gold = "#FFFF00"
